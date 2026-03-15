94c20a3d2ad4ea27688476d1985a6e0a6901f549
Alima-T <okhapka.a@gmail.com>
2026-03-16 00:46:56 +0100

    ex01 add implamentation of static functions isOperator and isDigit.

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,13 +6,13 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 22:33:44 by aokhapki          #+#    #+#             */
-/*   Updated: 2026/03/15 22:51:43 by aokhapki         ###   ########.fr       */
+/*   Updated: 2026/03/16 00:46:40 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-RPN::RPN(){}
+RPN::RPN() {}
 
 RPN::RPN(const RPN& src) : m_stack(src.m_stack) {}
 
@@ -23,4 +23,28 @@ RPN& RPN::operator=(const RPN& rhs)
 		m_stack = rhs.m_stack;
 	}
 	return *this;
+}
+
+RPN::~RPN() {}
+
+bool RPN::isOperator(const std::string& token)
+{
+	return token.size() == 1 && (token[0] =='+' || token[0] == '-' || token[0] == '*' || token[0] == '/');
+}
+
+//TODO: Handle empty strings, negative numbers and multi-digit numbers in isDigit
+bool RPN::isDigit(const std::string& token)
+{
+	return token.length() == 1 && token[0] >= '0' && token[0] <= '9';
+}
+
+void RPN::evaluateOperator(const std::string& op)
+{
+	//TODO: Implement the logic to pop the top two operands from the stack, perform the operation based on the operator, and push the result back onto the stack
+}
+
+//TODO: Handle division by zero and other edge cases in evaluateOperator
+void RPN::calculate(const std::string& expression)
+{
+	//TODO: Implement the logic to parse the expression, handle whitespace, and evaluate the RPN expression using the stack
 }
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 22:33:47 by aokhapki          #+#    #+#             */
-/*   Updated: 2026/03/15 22:50:06 by aokhapki         ###   ########.fr       */
+/*   Updated: 2026/03/16 00:43:32 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,20 @@ class RPN
 {
 private:
 	std::stack<long long> m_stack;
+	
+	static bool isOperator(const std::string& token);
+	static bool isDigit(const std::string& token);
+	//TODO: whitespace handling and error handling for invalid tokens
+	void evaluateOperator(const std::string& op);
+	
+	
 public:
 	RPN();
 	RPN(const RPN& src);
 	RPN& operator=(const RPN& rhs);
 	~RPN();
+	
+	void calculate(const std::string& expression);
 };
 
 #endif
